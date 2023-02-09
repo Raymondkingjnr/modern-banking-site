2b2f48853c032b3aa84d376ea209a2ae1e245b2b
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-02-09 23:24:58 +0100

    modified:   src/components/Billing.jsx 	modified:   src/components/Business.jsx 	modified:   src/components/Button.jsx 	modified:   src/components/CardDeal.jsx 	modified:   src/components/FeedBackCard.jsx 	modified:   src/components/GetStarted.jsx 	modified:   src/components/Hero.jsx 	modified:   src/components/Navbar.jsx 	modified:   src/components/Stats.jsx 	modified:   src/components/Testimonials.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins text-primary font-medium text-[18px] outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default Button;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
   const [toggle, setToggle] = React.useState(false);
   return (
     <nav className="w-full flex justify-between items-center py-6 navbar">
-      <img src={logo} alt="hoobank" className="w-[200px] h-[32px]" />
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className=" list-none sm:flex hidden justify-between items-center flex-1">
+      <ul className=" list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
@@ -30,9 +30,22 @@ function Navbar() {
 
         <div
           className={`${
-            toggle ? "flex" : "hideen"
+            toggle ? "flex" : "hidden"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-        ></div>
+        >
+          <ul className=" list-none flex  justify-center flex-col items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white mr-10 `}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
